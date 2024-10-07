c05c6dbdbca025df9827c34e2b11c60819e1b1c9
Sai Praveen Yerra <sai.praveen@capco.com>
2024-10-07 17:31:53 +0530

    Update spy MyCalculator.java

--- src/main/java/mypkg/MyCalculator.java
+++ src/main/java/mypkg/MyCalculator.java
@@ -8,7 +8,11 @@ public class MyCalculator {
 	 public int diff(int a, int b)
 	 {
 		 return a-b;
-	 }
+	  public int spydiv(int a, int b)
+	 {
+		 return a/b;
+	 }}
+	
 
 	public static void main(String[] args) {
 		
